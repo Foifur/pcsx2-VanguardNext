flex1988/pcsx2

GS: Add Pre-Round Sprite hack
This attempts to preproduce hardware behaviour, but falls down in a bunch of cases, hence the hack.

pcsx2/Pcsx2Config.cpp
@@ -332,6 +332,7 @@ Pcsx2Config::GSOptions::GSOptions()
 	Mipmap = true;
 	AA1 = true;
 	PointListPalette = false;
+	PreRoundSprites = false;
 
 	ManualUserHacks = false;
 	UserHacks_AlignSpriteX = false;
@@ -533,6 +534,7 @@ void Pcsx2Config::GSOptions::ReloadIniSettings()
 	GSSettingBool(OsdShowResolution);
 	GSSettingBool(OsdShowGSStats);
 	GSSettingBool(OsdShowIndicators);
+	GSSettingBoolEx(PreRoundSprites, "preround_sprites");
 
 	GSSettingBool(HWDisableReadbacks);
 	GSSettingBoolEx(AccurateDATE, "accurate_date");

Foifur/pcsx2-VanguardNext

pcsx2/Pcsx2Config.cpp
@@ -638,6 +638,7 @@ Pcsx2Config::GSOptions::GSOptions()
 	PreloadFrameWithGSData = false;
 	Mipmap = true;
 	HWMipmap = true;
+	PreRoundSprites = false;
 
 	ManualUserHacks = false;
 	UserHacks_AlignSpriteX = false;
@@ -833,6 +834,7 @@ void Pcsx2Config::GSOptions::LoadSave(SettingsWrapper& wrap)
 	SettingsWrapBitBool(OsdShowSettings);
 	SettingsWrapBitBool(OsdShowInputs);
 	SettingsWrapBitBool(OsdShowFrameTimes);
+	SettingsWrapBitBoolEx(PreRoundSprites, "preround_sprites");
 
 	SettingsWrapBitBool(HWSpinGPUForReadbacks);
 	SettingsWrapBitBool(HWSpinCPUForReadbacks);